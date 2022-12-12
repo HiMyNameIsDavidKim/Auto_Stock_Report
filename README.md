d5e839966987858cb8ef24a3855f2e5939894951
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2022-12-12 12:06:16 +0900

    12/12 study update

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Stock Report
+# Auto Stock Report
+* Automatic nasdaq's stock reporting system.
+
+## [Instructions]
+* way to crawl : Use Chrome Driver and selenium.
+* crawl from this site : www.investing.com
+* default stocks : AAPL, GOOGL, NVDA, TSLA, KO, PEP, ASML
+* default ETFs : QQQ, QLD, SPY
+
+## [How to Run?]
+* Open `stock_report.py` python file. Just run.
+* Saved file is here. `report` directory.
+
+## [Result]
+* Everyday, update today's stock price.
+    * You can check daily big moments.
+* When 52 week highest price is changed, update 52W stock price.
+    * You can check node that each low price. It can be your index to invest.
+* Every month start day, update this month's starting stock price.
+    * You can check today's price is how many diffrent from this month start day's price.
+
+## [Notice]
+* Stock prices are determined by `Standard Interest Rates` and `consumer psychology`. Don't decide to buy stock only check a relative price.
+* Don't make too much traffic from the site.
--- stock_report.py
+++ stock_report.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import openpyxl
 from selenium import webdriver
@@ -9,8 +10,8 @@ class Stock_report():
     def __init__(self):
         self.url = 'https://www.investing.com/equities/'
         self.url_etf = 'https://www.investing.com/etfs/'
-        self.symbol = []
-        self.symbol_etf = []
+        self.stock = []
+        self.stock_etf = []
         self.prices_now = []
         self.prices_52w = []
         self.prices_mon = []
@@ -18,25 +19,31 @@ class Stock_report():
         self.browser = webdriver.Chrome()
 
     def process(self):
-        self.symbol_req()
+        self.stock_req()
         self.crawl_prices()
         self.update_file()
 
-    def symbol_req(self):
-        # symbol = input('Please tell me the ticker of the stock to crawl. : ').split()
-        self.symbol = ['apple-computer-inc', 'google-inc', 'nvidia-corp', 'tesla-motors', 'coca-cola-co', 'pepsico', 'asml-holdings']
-        self.symbol_etf = ['powershares-qqqq', 'proshares-ultra-qqq-etf', 'spdr-s-p-500']
+    def stock_req(self):
+        # stock = input('Please tell me the stock to crawl. : ').split()
+        self.stock = ['apple-computer-inc',
+                       'google-inc', 'nvidia-corp',
+                       'tesla-motors', 'coca-cola-co',
+                       'pepsico',
+                       'asml-holdings']
+        self.stock_etf = ['powershares-qqqq',
+                           'proshares-ultra-qqq-etf',
+                           'spdr-s-p-500']
 
     def crawl_prices(self):
         browser = self.browser
-        symbol = self.symbol
-        symbol_etf = self.symbol_etf
+        stock = self.stock
+        stock_etf = self.stock_etf
         prices_now = self.prices_now
         prices_52w = self.prices_52w
         prices_mon = self.prices_mon
         xp_now = '//*[@id="__next"]/div/div/div/div[2]/main/div/div[1]/div[2]/div[1]/span'
         xp_52w = '//*[@id="__next"]/div/div/div/div[2]/main/div/div[1]/div[2]/ul/li[3]/div[2]'
-        for i in symbol:
+        for i in stock:
             browser.get(self.url + i)
             price_now = browser.find_element(By.XPATH, xp_now).text
             prices_now.append(float(price_now))
@@ -45,14 +52,14 @@ class Stock_report():
             prices_52w.append(float(price_52w))
         xp_now = '//*[@id="last_last"]'
         xp_52w = '//*[@id="leftColumn"]/div[9]/div[5]/span[2]'
-        for i in symbol_etf:
+        for i in stock_etf:
             browser.get(self.url_etf + i)
             price_now = browser.find_element(By.XPATH, xp_now).text
             prices_now.append(float(price_now))
             price_52w = browser.find_element(By.XPATH, xp_52w).text
             price_52w = price_52w[(price_52w.find('-') + 2):]
             prices_52w.append(float(price_52w))
-        for i in symbol:
+        for i in stock:
             browser.get(self.url + i + '-historical-data')
             date_choice = str(datetime.today())[5:7] + '/01/' + str(datetime.today())[:4]
             df = pd.read_html(browser.page_source)[1]
